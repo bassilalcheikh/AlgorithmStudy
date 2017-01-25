043054ab8f9fbb86bbc4b4821bbfc27de8b9a682
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-01-25 11:24:54 -0600

    Update EratoSieve.cs

--- EratoSieve.cs
+++ EratoSieve.cs
@@ -7,13 +7,15 @@ using System.Collections.Generic;
 public class EratoSieve{
     public static void Main(string[] args){
         List<int> desired_primes = EratoPrimes.GeneratePrimes(100);
-        Console.WriteLine("The End!");
+        foreach(int primenumber in desired_primes){
+          Console.WriteLine(primenumber);
+        }
   }
 }
 
 // traditional way (i.e., similar to Python)
 public class EratoPrimes{
-    public List<int> GeneratePrimes(int limit){
+    public static List<int> GeneratePrimes(int limit){
         // set up indexed array of candidates:
         int[,] first_array = new int[limit,2];
         for(int i=0; i<limit; i++){
@@ -23,7 +25,7 @@ public class EratoPrimes{
         first_array[0,1] = 0;
         first_array[1,1] = 0;
         // set up dynamic list of primes:
-        List<int> primes = new List<int>(){2};
+        List<int> primes = new List<int>();
         // begin Sieve:
         for(int a=2; a < limit; a++){
             if(first_array[a,1]==1){
@@ -32,10 +34,6 @@ public class EratoPrimes{
                     first_array[b,1]=0;
                 }
             }
-        }
-        // test to see indexed array:
-        for(int k=0; k<limit; k++){
-            Console.WriteLine(first_array[k,0]+" "+first_array[k,1]);
         }
         return primes;
     }
