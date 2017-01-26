f891e6e72efaa201e9d90662f689ffec947a4aed
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-01-25 22:36:26 -0600

    Update EratoSieve.cs

--- EratoSieve.cs
+++ EratoSieve.cs
@@ -3,34 +3,36 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public class EratoSieve{
     public static void Main(string[] args){
-        List<int> desired_primes = EratoPrimes.GeneratePrimes(100);
-        foreach(int primenumber in desired_primes){
-          Console.WriteLine(primenumber);
-        }
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        List<long> desired_primes = EratoPrimes.GeneratePrimes(10000);
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        /*foreach(int primenumber in desired_primes){
+            Console.WriteLine(primenumber);
+        }*/
   }
 }
 
-// traditional way (i.e., similar to Python)
 public class EratoPrimes{
-    public static List<int> GeneratePrimes(int limit){
-        // set up indexed array of candidates:
-        int[,] first_array = new int[limit,2];
-        for(int i=0; i<limit; i++){
+    public static List<long> GeneratePrimes(long limit){
+        long[,] first_array = new long[limit,2];
+        for(long i=0; i<limit; i++){
             first_array[i,0] = i;
             first_array[i,1] = 1;
         }
         first_array[0,1] = 0;
         first_array[1,1] = 0;
-        // set up dynamic list of primes:
-        List<int> primes = new List<int>();
-        // begin Sieve:
-        for(int a=2; a < limit; a++){
+
+        List<long> primes = new List<long>();
+
+        for(long a=2; a < limit; a++){
             if(first_array[a,1]==1){
                 primes.Add(first_array[a,0]);
-                for(int b = a*a; b < limit; b+=a){
+                for(long b = a*a; b < limit; b+=a){
                     first_array[b,1]=0;
                 }
             }
