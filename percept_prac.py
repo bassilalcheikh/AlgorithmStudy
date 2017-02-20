82e0895e7d5a0acb138c39995f1970527a9db18f
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-02-20 15:42:57 -0600

    Removed plot lines, adjusted initial weights

--- percept_prac.py
+++ percept_prac.py
@@ -4,20 +4,14 @@
 #
 import numpy as np
 import matplotlib.pyplot as mpl
-#from Perceptron import perceptron
 
-p = np.arange(-10,10,1)
-mpl.axis([0, 10, 0, 10])
-
-X_data = np.array([[2.7810836,2.550537003], [1.465489372,2.362125076], [3.396561688,4.400293529], [1.38807019,1.850220317], [3.06407232,3.005305973], [7.627531214,2.759262235], [5.332441248,2.088626775], [6.922596716,1.77106367], [8.675418651,-0.242068655], [7.673756466,3.508563011]])
+#X_data = np.array([[2.7810836,2.550537003], [1.465489372,2.362125076], [3.396561688,4.400293529], [1.38807019,1.850220317], [3.06407232,3.005305973], [7.627531214,2.759262235], [5.332441248,2.088626775], [6.922596716,1.77106367], [8.675418651,-0.242068655], [7.673756466,3.508563011]])
 #X_data = np.array([[0,4],[-5,0],[-6,1],[1,4],[4,5],[-3,2],[-2,5],[0,0],[1,0],[3,1],[5,2],[-1,-1],[-2,-3],[-5,-5]])
 #X_data = np.array([[1,1],[2,-2],[-1,-1.5],[-2,1],[1.5,-0.5]])
-Y_data = [-1,-1,-1,-1,-1,1,1,1,1,1]
+#Y_data = [-1,-1,-1,-1,-1,1,1,1,1,1]
 #Y_data = [1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1]
 #Y_data = [1,-1,-1,1,1]
 x, y = zip(*X_data)
-
-#mpl.scatter(x, y)
 
 def linear_form2(weights):
     # input: weights a,b,c such that a+bx+cy = 0
@@ -30,7 +24,7 @@ def linear_form2(weights):
 def linear_perceptron(X, Y, r): # inputs: X = learning points, Y = binary values, r = learning rate
     d = X.shape[1] #dimensions
     N = X.shape[0] # number of points
-    W = np.transpose(np.insert(([0]*d),d,1)) # weights
+    W = np.transpose(np.insert(([0.5]*d),d,1)) # weights
     X = np.insert(X, 0, 1, axis=1) # insert '1' into x_0
     sign = lambda x: 1 if x>0  else -1
     correct_guesses = 0
@@ -45,11 +39,4 @@ def linear_perceptron(X, Y, r): # inputs: X = learning points, Y = binary values
 
 #w_ = linear_perceptron(X_data, Y_data, 0.1)
 
-#mpl.plot((float(1*w[1])/w[2]*p)+(-45*float(w[0])/w[2]), 'r')
-#print(w_)
-
-#mpl.plot((float(-2)/9)-(float(1)/3)*p, 'r')
-mpl.plot(2*p, 'r')
-
 #print(linear_form2(w_))
-mpl.show()
