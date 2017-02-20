e40370a3564690ef5233836f50c1da2f8cbd10fc
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-02-20 07:48:49 -0600

    Update percept_prac.py

--- percept_prac.py
+++ percept_prac.py
@@ -4,44 +4,52 @@
 #
 import numpy as np
 import matplotlib.pyplot as mpl
-from Perceptron import perceptron
+#from Perceptron import perceptron
 
-X = np.arange(0,10,1)
+p = np.arange(-10,10,1)
 mpl.axis([0, 10, 0, 10])
 
-#Y = perceptron(data_, Y_3, 1000)
-#print(Y[0][0], Y[0][1], Y[1])
+X_data = np.array([[2.7810836,2.550537003], [1.465489372,2.362125076], [3.396561688,4.400293529], [1.38807019,1.850220317], [3.06407232,3.005305973], [7.627531214,2.759262235], [5.332441248,2.088626775], [6.922596716,1.77106367], [8.675418651,-0.242068655], [7.673756466,3.508563011]])
+#X_data = np.array([[0,4],[-5,0],[-6,1],[1,4],[4,5],[-3,2],[-2,5],[0,0],[1,0],[3,1],[5,2],[-1,-1],[-2,-3],[-5,-5]])
+#X_data = np.array([[1,1],[2,-2],[-1,-1.5],[-2,1],[1.5,-0.5]])
+Y_data = [-1,-1,-1,-1,-1,1,1,1,1,1]
+#Y_data = [1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1]
+#Y_data = [1,-1,-1,1,1]
+x, y = zip(*X_data)
 
-#mpl.plot(X, 2*X+1, 'r', 2*X-1, 'b'), (-1)*(Y[0][0])/Y[0][1]*X-(Y[1])/Y[0][1], 'g')
-#mpl.show()
+#mpl.scatter(x, y)
 
-X_data = np.array([[0,4],[-5,0],[-6,1],[1,4],[4,5],[-3,2],[-2,5],[0,0],[1,0],[3,1],[5,2],[-1,-1],[-2,-3],[-5,-5]])
-Y_data = np.array([1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1])
-
-
-#print X_data.shape[0]
-#print Y_data.shape[0]
+def linear_form2(weights):
+    # input: weights a,b,c such that a+bx+cy = 0
+    # returns coefficients a,b such that y = ax+b
+    a = float(-weights[1])/weights[2]
+    b = float(-weights[0])/weights[2]
+    return [a,b]
 
 #input format: X is a column
-def linear_perceptron(X, Y, N):
-    # prepare X and W arrays
-    W = np.transpose([0]*(X.shape[1]+1))
-    X = np.insert(X, 0, 1, axis=1)
-
-    point_ct = X.shape[0]
-
-    mispl_pts = 1
-
-    while mispl_pts > 0:
-        for t in range(0, point_ct):
-            h = np.sign(W*X)
-            if h < 1:
-                mispl_pts += 1
-                W = W + N*Y[t]*X[t]
+def linear_perceptron(X, Y, r): # inputs: X = learning points, Y = binary values, r = learning rate
+    d = X.shape[1] #dimensions
+    N = X.shape[0] # number of points
+    W = np.transpose(np.insert(([0]*d),d,1)) # weights
+    X = np.insert(X, 0, 1, axis=1) # insert '1' into x_0
+    sign = lambda x: 1 if x>0  else -1
+    correct_guesses = 0
+    while correct_guesses < N:
+        for t in range(0,N):
+            h = sign(np.dot(W, X[t]))
+            if h != Y[t]:
+                W = W + r*np.dot(Y[t],X[t])
             else:
-                mispl_pts -= 1
+                correct_guesses += 1
+    return W
 
+#w_ = linear_perceptron(X_data, Y_data, 0.1)
 
+#mpl.plot((float(1*w[1])/w[2]*p)+(-45*float(w[0])/w[2]), 'r')
+#print(w_)
 
+#mpl.plot((float(-2)/9)-(float(1)/3)*p, 'r')
+mpl.plot(2*p, 'r')
 
-linear_perceptron(X_data, Y_data, 0.2)
+#print(linear_form2(w_))
+mpl.show()
