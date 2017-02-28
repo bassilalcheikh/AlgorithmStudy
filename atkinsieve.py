96ab8128021cc4ebca668d5b8aa2bd8aef80fee3
bassilalcheikh <bassil.alcheikh@gmail.com>
2017-02-28 02:20:36 -0600

    Update atkinsieve.py

--- atkinsieve.py
+++ atkinsieve.py
@@ -28,16 +28,19 @@ results = [2,3,5]
 sieve_list = [False]*limit
 
 x = 1; y = 1
-while(x*x < limit):
-    while(y*y < limit):
+while(x*x < limit**0.5+1):
+    while(y*y < limit**0.5+1):
         n = (4*x*x)+(y*y)
-        if
+        if n <= limit and (n%12==5 or n%12==1):
+            sieve_list[]=True
 
         n -= x*x
-        if
+        if n <= limit and n%12==7:
+            sieve_list[]=True
 
         n -= 2*y*y
-        if
+        if n <= limit and n%12==11 and x>y:
+            sieve_list[]=True
 
         y += 1
     x += 1
